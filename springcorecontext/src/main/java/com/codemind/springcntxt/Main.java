a63cd76e80b120821ce58765c16fa04c5d6b78f6
Priyask22 <120859517+Priyask22@users.noreply.github.com>
2023-12-04 19:31:40 +0530

    spring

---
+++ springcorecontext/src/main/java/com/codemind/springcntxt/Main.java
@@ -0,0 +1,37 @@
+package com.codemind.springcntxt;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext cntxt = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		//Employee emp = cntxt.getBean(Employee.class);
+		
+		Employee emp1 = (Employee) cntxt.getBean("rohit");
+		Employee emp2 = (Employee) cntxt.getBean("rohit");
+		
+	   
+		/** Scopes of the Bean
+		 *   1. Prototype
+		 *   2. Singleton
+		 *   
+		 */
+		
+		/**
+		 * Auto wiring  
+		 * 
+		 * 
+		 */
+		
+			
+		System.out.println(emp1);
+		
+		
+	}
+
+}
